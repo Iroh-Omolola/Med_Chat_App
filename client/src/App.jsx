865f9ed9c2e803a17a38b397b78a0a0f3d78ef45
OMOLOLA7991 <irohomolola@gmail.com>
2021-12-10 21:13:29 +0100

    create-app

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {StreamChat} from 'stream-chat'
+import {Chat, Channel} from 'stream-chat-react'
+import Cookies from 'universal-cookie'
+import ChannelContainer from './components/ChannelContainer'
+import ChannelListContainer from './components/ChannelListContainer'
+
+const apikey= 'ykew4bjdxh3a'
+const client = StreamChat.getInstance(apikey)
+
+const App = () => {
+    return (
+        <div className="app_wrapper">
+            <Chat client={client} theme='team light'>
+              <ChannelListContainer
+
+              />
+              <ChannelContainer
+              />
+            </Chat>
+        </div>
+    )
+}
+
+export default App
